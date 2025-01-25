88e13e4a47b8d9d1fd8edba8e38ad5d5c5355907
PrasannakumaraP <2023AA05973@wilp.bits-pilani.ac.in>
2025-01-25 19:01:49 +0530

    Import library order update

--- src/train.py
+++ src/train.py
@@ -8,12 +8,12 @@ import os
 import pickle
 import numpy as np
 import pandas as pd
-import mlflow
 from mlflow import sklearn  # For scikit-learn models
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error,accuracy_score
 from joblib import dump
+import mlflow
 
 def load_data(path):
     """
