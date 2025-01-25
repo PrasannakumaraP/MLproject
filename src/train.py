c9c7687c2b5220c087eee37f5abc17b673da7112
PrasannakumaraP <2023AA05973@wilp.bits-pilani.ac.in>
2025-01-25 19:04:26 +0530

    Removed the unused import library

--- src/train.py
+++ src/train.py
@@ -8,7 +8,6 @@ import os
 import pickle
 import numpy as np
 import pandas as pd
-from mlflow import sklearn  # For scikit-learn models
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error,accuracy_score
