86156a95787eb8f6bf267dcf85b14c135e074bef
PrasannakumaraP <2023AA05973@wilp.bits-pilani.ac.in>
2025-01-25 19:21:15 +0530

    Removed unused accuracy_score

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error,accuracy_score
+from sklearn.metrics import mean_squared_error
 from joblib import dump
 import mlflow
 
